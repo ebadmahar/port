e67f052b3415caad9ef23c549a3829e9598acfd8
Ebad <attitudeboys.official@gmail.com>
2025-08-18 10:01:26 +0500

    Updated image in portfolio

--- netlify/functions/sendMail.js
+++ netlify/functions/sendMail.js
@@ -1,7 +1,7 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
 exports.handler = async (event, context) => {
-  const { name, email, message } = JSON.parse(event.body)
+  const { name, email, message } = JSON.parse(event.body);
 
   let transporter = nodemailer.createTransport({
     service: "gmail",
@@ -9,25 +9,31 @@ exports.handler = async (event, context) => {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-  })
+  });
 
   const mailOptions = {
     from: email,
     to: process.env.EMAIL_TO,
     subject: `New Contact Form Message from ${name}`,
-    text: message,
-  }
+    text: `
+You have received a new message from your portfolio contact form:
+
+Name: ${name}
+Email: ${email}
+Message: ${message}
+    `,
+  };
 
   try {
-    await transporter.sendMail(mailOptions)
+    await transporter.sendMail(mailOptions);
     return {
       statusCode: 200,
       body: JSON.stringify({ success: true }),
-    }
+    };
   } catch (err) {
     return {
       statusCode: 500,
       body: JSON.stringify({ success: false, error: err.message }),
-    }
+    };
   }
-}
+};
